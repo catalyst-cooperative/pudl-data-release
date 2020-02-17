c8120b350484d0172222fb7aebe352c16f243fd3
Zane Selvans <zane.selvans@catalyst.coop>
2020-02-17 16:43:20 -0600

    Fix bug in command line args for pudl_data

--- data-release.sh
+++ data-release.sh
@@ -13,7 +13,7 @@ START_TIME=$(date --iso-8601="seconds")
 EIA_YEARS="--years 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018"
 # Default (with no args) is to load all available data:
 EPACEMS_YEARS=""
-EPACEMS_STATES="CO ID"
+EPACEMS_STATES="--states CO ID"
 FERC1_YEARS1=""
 
 ###############################################################################
